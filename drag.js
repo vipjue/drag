c32b73d5be17f5a8fc3f6693cc00e1c0b8bec45e
vipjue <15911007761@163.com>
2018-06-14 00:10:44 +0800

    change drag.js from_master

--- drag.js
+++ drag.js
@@ -5,5 +5,9 @@ function Drag(id){
     obj.onmousedown=function(ev){
         disX = ev.pageX-obj.offsetLeft;
         disY = ev.pageY-obj.offsetTop;
+        document.onmousemove=function(ev){
+        	obj.style.left = ev.pageX-disX+"px";
+        	obj.style.top = ev.pageY-disY+"px";
+        }
     }
 }
